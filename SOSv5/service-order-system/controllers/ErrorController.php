8314d7aef77b41eb80c119ba96ba3c2c7c60bb7d
08EAGM94 <08edgar.allan94@gmail.com>
2026-06-10 12:20:41 -0600

    delete duplications

--- SOSv5/service-order-system/controllers/ErrorController.php
+++
@@ -1,8 +0,0 @@
-<?php
-    /*El controlador Error es el más simple del proyecto, solo contiene el método index el cual importa una vista 
-     * html con el mensaje "La página que buscas no existe"*/
-    class ErrorController{
-        public function index(){
-            require_once '../views/error.php';
-        }
-    }
--- SOSv5/service-order-system/views/finishingLayouts/htmlSides/footer.php
+++
@@ -1,4 +0,0 @@
-<!-- Este archivo es importante para cerrar la semantica html del index de la carpeta finishing -->
-    </body>
-</html>
-
